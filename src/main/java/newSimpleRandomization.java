1d2e1685b9ab4c3f78d7bf1b73d4965940298862
Vitaly <Vitaly1Black@yandex.ru>
2022-06-17 14:38:14 +0300

    previous commit fix

---
+++ src/main/java/newSimpleRandomization.java
@@ -0,0 +1,75 @@
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+
+public class newSimpleRandomization {
+
+	static EasyRandom g = new EasyRandom();
+
+	public static class Address { 
+
+		static SimpleRandomization.Address internal;
+
+		Address(SimpleRandomization.Address internal) { this.internal = internal; }
+
+		public String toString() { 
+			String r = internal.toString();
+			return r;
+		}
+
+	} 
+
+	public static class Data { 
+
+		static SimpleRandomization.Data internal;
+
+		Data(SimpleRandomization.Data internal) { this.internal = internal; }
+
+		public String toString() { 
+			String r = internal.toString();
+			return r;
+		}
+
+	} 
+
+	public static class Job { 
+
+		static SimpleRandomization.Job internal;
+
+		Job(SimpleRandomization.Job internal) { this.internal = internal; }
+
+		public String toString() { 
+			String r = internal.toString();
+			return r;
+		}
+
+	} 
+
+	public static class Person { 
+
+		static SimpleRandomization.Person internal;
+
+		Person(SimpleRandomization.Person internal) { this.internal = internal; }
+
+		public String toString() { 
+			String r = internal.toString();
+			return r;
+		}
+
+	} 
+
+	public static class Execution { 
+
+		static SimpleRandomization.Execution internal;
+
+		Execution(SimpleRandomization.Execution internal) { this.internal = internal; }
+
+		public static Data setAndReturnData() { 
+			SimpleRandomization.Data r = internal.setAndReturnData();
+			r = g.nextObject(SimpleRandomization.Data.class);
+			return new Data(r);
+		}
+
+	} 
+
+
+}
